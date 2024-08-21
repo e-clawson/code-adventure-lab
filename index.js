c721cd58b878c3dd75f6379086fed2d838583403
Elizabeth Clawson <emclawson1@gmail.com>
2024-08-21 14:31:43 -0500

    added adventurer and companion classes

--- index.js
+++ index.js
@@ -21,6 +21,8 @@ let inventoryItems = console.log(adventurer.inventory)
 
 console.log(adventurer.roll())
 
+// part 2 
+
 class Character {
     constructor (name) {
       this.name = name;
@@ -41,3 +43,37 @@ robin.companion.companion = new Character("Frank");
 robin.companion.companion.type = "Flea";
 robin.companion.companion.inventory = ["small hat", "sunglasses"];
 console.log(robin)
+
+// part 3 - class features 
+class Adventurer extends Character {
+    constructor (name, role) {
+      super(name);
+      // Adventurers have specialized roles.
+      this.role = role;
+      // Every adventurer starts with a bed and 50 gold coins.
+      this.inventory.push("bedroll", "50 gold coins");
+    }
+    // Adventurers have the ability to scout ahead of them.
+    scout () {
+      console.log(`${this.name} is scouting ahead...`);
+      super.roll();
+    }
+  }
+
+class Companion extends Character { 
+    constructor (name,type, inventory = []){
+        super(name);
+        this.type = type;
+        this.inventory = inventory;
+    }
+}
+
+let robin2 = new Adventurer("robin", "human", ["sword", "potion", "artifact"]);
+
+let leo = new Companion("leo", "cat", ["boots"]);
+
+let frank = new Companion("frank", "flea", ["small hat", "sunglasses"]);
+
+console.log(robin2, leo, frank)
+
+
